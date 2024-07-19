dfd11cad7f93fe8ed0430dcb580378dadeccd35d
ainnot <stkim2007@gmail.com>
2024-07-20 01:55:53 +0900

    start scene
    almost finish

--- HackerthonGame/Assets/Scripts/UI/UI.cs
+++ HackerthonGame/Assets/Scripts/UI/UI.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     bool isPanelActive = false;
+    bool isInfoPanelActive = false;
     public GameObject panel;
+    public GameObject InfoPanel;
     public Slider slider;
     public void Quit(){
         Application.Quit();
     }
     public void Play(){
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene("GameScene");
     }
     public void PanelQuit(){
         panel.SetActive(false);
         isPanelActive=false;
+    }
+    public void InfoPanelQuit(){
+        InfoPanel.SetActive(false);
+        isInfoPanelActive=false;
     }
     public void PanelActive(){
         if(isPanelActive){
@@ -28,19 +34,35 @@ public class UI : MonoBehaviour
             isPanelActive = true;
         }
     }
+    public void InfoPanelActive(){
+        if(isInfoPanelActive){
+            InfoPanel.SetActive(false);
+            isInfoPanelActive = false;
+        }
+        else{
+            InfoPanel.SetActive(true);
+            isInfoPanelActive = true;
+        }
+    }
     void Start(){
         Debug.Log(1);
         //SoundManager.Instance.BackgroundPlay("1",100);
     }
     void Update(){
         if(Input.GetKeyDown(KeyCode.Escape)){
-            if(isPanelActive){
+            if(!isInfoPanelActive){
+                if(isPanelActive){
                 panel.SetActive(false);
                 isPanelActive = false;
-            }
-            else{
+                }
+                else{
                 panel.SetActive(true);
                 isPanelActive = true;
+                }
+            }
+            if(isInfoPanelActive){
+                InfoPanel.SetActive(false);
+                isInfoPanelActive = false;
             }
         }
     }
