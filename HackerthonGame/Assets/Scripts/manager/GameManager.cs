90307b91f36dc42e1a2f81134473c4185fb35a78
ainnot <stkim2007@gmail.com>
2024-07-20 02:28:08 +0900

    add gamemanager

---
+++ HackerthonGame/Assets/Scripts/manager/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<SoundManager>
+{
+    public GameObject Player;
+    [SerializeField] float Time;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
