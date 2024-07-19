06ba8e57f1334389e44e73f18451f526c0fdd862
roy0206 <swimkinig_ryu@naver.com>
2024-07-19 09:25:44 +0900

    세이브기능

---
+++ HackerthonGame/Assets/Scripts/Save/Database.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class Database
+{
+    public int count;
+    public TestClass testClass;
+
+
+
+
+
+
+
+
+
+    public Database()
+    {
+        count = 0;
+    }
+}
